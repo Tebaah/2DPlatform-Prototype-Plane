3291e71e951ee71b5b4e40bf68079231bbf22304
Tebaah <e2nd@protonmail.com>
2024-02-11 21:36:29 -0300

    feat(SemiBoss):semi boss programming is completed

--- Scrips/SemiBoss.cs
+++ Scrips/SemiBoss.cs
@@ -8,6 +8,10 @@ public partial class SemiBoss : Area2D
     private CollisionShape2D _myCollision;
     private bool _isDead = false;
     private Global _global;
+    [Export]public PackedScene bulletEnemy;
+    private bool _canShoot = true;
+    private Marker2D _spawnBulletEnemy;
+    private int _life = 5;
 
     // Methods
 
@@ -15,6 +19,7 @@ public partial class SemiBoss : Area2D
     {
         _myCollision = GetNode<CollisionShape2D>("CollisionShape2D");
         _spriteController = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+        _spawnBulletEnemy = GetNode<Marker2D>("SpawnBullet");
         _global = GetNode<Global>("/root/Global");
     }
 
@@ -26,20 +31,27 @@ public partial class SemiBoss : Area2D
             Position += new Vector2(0, 1);
         }
 
+        Shoot();
     }
 
-       public async void OnBodyEnteredShip(Area2D area)
+    public async void OnBodyEnteredShip(Area2D area)
     {
         if(area.IsInGroup("Bullet"))
         {
-            _myCollision.Visible = false;   
-            _spriteController.Play("death");
-
-            _global.pointsPlayer += 10;
+            _life -= 1;
             
-            _isDead = true;
-            await ToSignal(GetTree().CreateTimer(0.3),"timeout");
-            QueueFree();
+            if(_life == 0)
+            {
+                _myCollision.Visible = false;   
+                _spriteController.Play("death");
+
+                _global.pointsPlayer += 10;
+                
+                _isDead = true;
+                await ToSignal(GetTree().CreateTimer(0.3),"timeout");
+                QueueFree();
+            }
+
         }
     }
     public async void OnBodyEntered(CharacterBody2D body)
@@ -51,6 +63,24 @@ public partial class SemiBoss : Area2D
             _isDead = true;
             await ToSignal(GetTree().CreateTimer(0.3),"timeout");
             QueueFree();
+        }
+    }
+
+        public async void Shoot()
+    {
+        if(_isDead == false)
+        {
+            if(_canShoot == true)
+            {
+                Area2D newBulettEnemy = (Area2D)bulletEnemy.Instantiate();
+                newBulettEnemy.GlobalPosition = _spawnBulletEnemy.GlobalPosition;
+                GetParent().AddChild(newBulettEnemy);
+
+                _canShoot = false;
+
+                await ToSignal(GetTree().CreateTimer(2.0f), "timeout");
+                _canShoot = true;
+            }
         }
     }
 }
